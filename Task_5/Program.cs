3628bd744d66339d3524fec1373f2cc9dbb6f7fc
lokman <kalinin5000@yandex.ru>
2022-12-05 00:11:09 +0300

    Решение пятой задачи

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,32 +1,33 @@
-﻿Console.WriteLine("Введите число.");
-try
+﻿try
 {
+    Console.WriteLine("Введите число.");
+
     int num = Convert.ToInt32(Console.ReadLine());
+    int save = num;
+    int rev = 0;
+    if (num < 99)
+    {
+        Console.WriteLine("Введено некорректное число.");
+    }
+    else
+    {
+        while (num > 0)
+        {
+            int ost = num % 10;
+            rev = rev * 10 + ost;
+            num = num / 10;
+        }
+        if (save == rev)
+        {
+            Console.WriteLine($"Число {save} является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine($"Число {save} не является палиндромом.");
+        }
+    }
 }
 catch
 {
     Console.WriteLine("Ошибка ввода! Введите целое число.");
-}
-int save = num;
-int rev = 0;
-if (num < 99)
-{
-    Console.WriteLine("Введено некорректное число.");
-}
-else
-{
-    while (num > 0)
-    {
-        int ost = num % 10;
-        rev = rev * 10 + ost;
-        num = num / 10;
-    }
-    if (save == rev)
-    {
-        Console.WriteLine($"Число {save} является палиндромом.");
-    }
-    else
-    {
-        Console.WriteLine($"Число {save} не является палиндромом.");
-    }
 }
