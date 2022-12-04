c9b4f009a42a373d242cbb84e19954a390197099
lokman <kalinin5000@yandex.ru>
2022-12-05 00:11:02 +0300

    Решение третей задачи

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,26 +1,27 @@
 ﻿Console.WriteLine("Введите число.");
 try
 {
-int num = Convert.ToInt32(Console.ReadLine());
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    int st = 3;
+    int Exponentiation(int save, int degree)
+    {
+        int deg = 0;
+        int num = save;
+        for (deg = 2; deg <= degree; deg++)
+        {
+            num = num * save;
+        }
+        return num;
+    }
+    int save = num;
+    for (int i = 1; i <= save; i++)
+    {
+        int res = Exponentiation(i, st);
+        Console.Write($"{res} ");
+    }
 }
 catch
 {
     Console.WriteLine("Ошибка ввода! Введите целое число.");
-}
-int st = 3;
-int Exponentiation (int save, int degree)
-{
-    int deg = 0;
-    int num = save;
-    for (deg = 2; deg <= degree; deg++ )
-    {
-         num = num * save;
-    }
-    return num;
-}
-int save = num;
-for (int i = 1; i <= save; i ++)
-{
-    int res = Exponentiation(i, st);
-    Console.Write($"{res} ");
 }
