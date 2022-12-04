2b098abcd9f7161767b9b26dcba38611748db026
lokman <kalinin5000@yandex.ru>
2022-12-04 23:47:12 +0300

    Решение третей задачи

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число.");
+int num = Convert.ToInt32(Console.ReadLine());
+int st = 3;
+int Exponentiation (int save, int degree)
+{
+    int deg = 0;
+    int num = save;
+    for (deg = 2; deg <= degree; deg++ )
+    {
+         num = num * save;
+    }
+    return num;
+}
+int save = num;
+for (int i = 1; i <= save; i ++)
+{
+    int res = Exponentiation(i, st);
+    Console.Write($"{res} ");
+}
